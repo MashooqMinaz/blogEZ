13cc70e5cc474a9363e9eaac92baa2031a2c65e8
MashooqMinaz <mashooqminaz@gmail.com>
2023-06-22 14:35:28 +0530

    blog app v-1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoryNavbarComponent } from './components/category-navbar/category-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './components/pages/terms-and-condition/terms-and-condition.component';
+import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
+import { CommentsFormComponent } from './components/comments/comments-form/comments-form.component';
+import { CommentsListComponent } from './components/comments/comments-list/comments-list.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionComponent,
+    SubscriptionFormComponent,
+    CommentsFormComponent,
+    CommentsListComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
